e8d41c6fdb5201b51292ca9521bc7f359532f796
inlineflow <thrashdev@protonmail.com>
2025-06-27 17:13:02 +0600

    updated config

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,21 @@
 // jest.config.ts
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-  preset: 'ts-jest', // Enables TypeScript support in Jest
-  testEnvironment: 'jsdom', // Sets up a browser-like environment
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts', '<rootDir>/test/matchers/domMatchers.ts'], // Points to the setup file for Jest-DOM matchers
+  preset: "ts-jest", // Enables TypeScript support in Jest
+  testEnvironment: "jsdom", // Sets up a browser-like environment
+  restoreMocks: true,
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.ts",
+    "<rootDir>/test/matchers/domMatchers.ts",
+  ], // Points to the setup file for Jest-DOM matchers
   moduleNameMapper: {
     // '\\.(css|scss)$': 'identity-obj-proxy', // Mocks CSS/SCSS imports
-    '\\.(css|scss)$': '<rootDir>/test/__mocks__/styleMock.ts', // Mocks CSS/SCSS imports
-    '\\.(svg)$': '<rootDir>/__mocks__/svgMock.ts', // Mocks SVG imports (create this file)
+    "\\.(css|scss)$": "<rootDir>/test/__mocks__/styleMock.ts", // Mocks CSS/SCSS imports
+    "\\.(svg)$": "<rootDir>/__mocks__/svgMock.ts", // Mocks SVG imports (create this file)
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', // Transforms TypeScript and TSX files
+    "^.+\\.(ts|tsx)$": "ts-jest", // Transforms TypeScript and TSX files
   },
 };
 
